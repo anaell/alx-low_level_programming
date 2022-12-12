2a1179127e94e35faa8e066500fe72152848b963
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-11 18:34:25 -0800

    asew

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this main is for printing a line.
+ * You should always endeavor and make sure
+ * to always follow the rules in character length
+ * in a single line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
