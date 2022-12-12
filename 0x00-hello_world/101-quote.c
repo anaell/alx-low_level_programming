75783fa2f0dde608c722b871bc5c99e9f569b2af
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-11 19:40:18 -0800

    galkja

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,19 +5,16 @@
 
 
 /**
- * main - print quote
+ * main - The function print a quote
  * 
+ * This code prints Dora's quote.
+ * Always adhere to the rule max numbe of characters.
+ * Things can happen and you'll not keep it though.
  *
- *
- * Description: prints Dora's quote
- *
- *
- *
- * Return: Always 1 (Success)
- *
+ * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
