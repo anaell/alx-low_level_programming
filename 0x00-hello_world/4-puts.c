fd5db9b794d1184961d6f17851645455d09a13be
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-11 17:37:46 -0800

    er

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/* This main functions prints out that sentences
+/**
+ *This main functions prints out that sentences
  *and returns 0.
  */
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return(0);
 }
