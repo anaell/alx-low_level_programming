2b6fd2adc43ac6b3be412e0f263fc13f72edb9f9
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-12 16:45:13 -0800

    dfs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,20 +15,20 @@
 */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
-		printf("%d is positive", n); 
-	}
-	if (n<0)
-	{
-		printf("%d is negative", n);
-	}
-	if (n==0)
-	{
-		printf("%d is zero", n);
-	}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n>0)
+{
+printf("%d is positive\n", n);
+}
+if (n<0)
+{
+printf("%d is negative\n", n);
+}
+if (n==0)
+{
+printf("%d is zero\n", n);
+}
+return (0);
 }
