fab9e49b7c4b14aa679be13ce445b0aa7cb72903
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-12 18:35:17 -0800

    sda

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints all the lower and
+ * upper case letters.
+ *
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+int l;
+int u;
+l = 'a';
+u = 'A';
+while (l <= 'z')
+{
+	putchar(l);
+	l++;
+}
+while (u <= 'Z')
+{
+	putchar(u);
+	u++;
+}
+putchar("\n");
+return 0;
+}
