ccd52a100daba0e9fc6287d96ec5361d91e99c47
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-12 18:07:48 -0800

    rtre

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,9 @@ else if (n % 10 < 6)
 {
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
-	
-	return (0);
+else (n % 10 > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+}
+return (0);
 }
