0ad40ad9815d018c8ca5634be9d59da484d0de20
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-14 18:39:12 -0800

    new

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - function to check the sign of a number.
+ *
+ * @e: the test number
+ *
+ * Return: (1)
+ */
+int print_sign(int e)
+{
+	if (e > 0)
+		return (1);
+	else if (e = 0)
+		return (0);
+	else
+		return (-1);
+}
