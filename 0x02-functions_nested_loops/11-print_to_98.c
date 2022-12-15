d7176d3ae459b756fa0408150014810645b54d52
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-15 14:15:23 -0800

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,23 +13,19 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
-		printf("\n");
 	}
 	else if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
-			printf("\n");
 		}
 	}
-	else
-	{
-		printf("98\n");
+	printf("98\n");
 }
