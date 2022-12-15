6fe623ce5b06dfe72c0729753f8f4ef48885a0e4
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-14 18:55:23 -0800

    new

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,22 @@ int print_sign(int e)
 {
 	if (e > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
 	else if (e == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		return (0);
 	}
 	else if (e < 0)
 	{
-		_putchar("-");
+		_putchar('-');
 		return (-1);
 	}
 	else
 	{
-		_putchar("-");
+		_putchar('-');
 		return ('/');
 
 	}
