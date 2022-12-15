94dcfa0b93eafd5361fbbf1b08cd3e430f078c32
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-14 19:18:10 -0800

    new

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - this function prints the last digit of a number.
+ *
+ * @x: number to be tested.
+ *
+ * Return: (0)
+ */
+int print_last_digit(int x);
+{
+	_putchar(x % 10);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,5 @@ void print_alphabet_x10(void);
 int _islower(int m);
 int _isalpha(int a);
 int print_sign(int e);
-int _abs(int);
+int _abs(int f);
+int print_last_digit(int x);
