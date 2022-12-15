61df6bf2f719e43d88c2a715e56ecbeb9c143c02
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-14 18:15:47 -0800

    new

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,8 @@
  * _islower - function that checks whether a letter is lowercase or not,
  * returns 1 if lower else 0.
  *
+ * @m - character to test.
+ *
  * Returns: (0)
  */
 int _islower(int m)
