f608903bed1249ae0b2d18917cd727adbe05d3a5
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-14 19:05:04 -0800

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function that prints the absolute value of an integer.
+ *
+ * @f: test number.
+ *
+ * Return: (0)
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = int * -1;
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,4 @@ void print_alphabet_x10(void);
 int _islower(int m);
 int _isalpha(int a);
 int print_sign(int e);
+int _abs(int);
