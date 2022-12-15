7eff6e28aae121311daa3a5e89b1c6360216e28f
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-15 15:26:48 -0800

    new

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -12,5 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-        return (0);
+	return (0);
 }
