16f44ef7f49fbe11ee4c4732df97273faf817596
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-14 16:10:22 -0800

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char tar[] = "_putchar";
+	int tar[] = "_putchar";
 	int i;
 	for (i = 0; i < 8; i++)
 		{
 		_putchar(tar[i]);
 		}
-	_putchar("\n");
+	_putchar('\n');
 	return (0);
 }
