fbec8a6197cbd19796c6cfa8e0044d49c1c83edf
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-14 17:21:28 -0800

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * main - prints all lowercase letters.
+ * print_alphabet - prints all lowercase letters.
  *
  * Return: (0)
  */
 void print_alphabet(void)
 {
 	int p;
+
 	p = 'a';
+
 	while (p <= 'z')
 	{
 		_putchar(p);
