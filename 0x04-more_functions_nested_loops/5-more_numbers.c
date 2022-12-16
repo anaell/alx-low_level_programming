35c7fcc3d3ad2c25da194a74ac094382910ca60f
Anaell <chimereanaele@gmail.com>
2022-12-16 22:11:44 +0100

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,5 +2,20 @@
 
 /**
  * more_numbers - this function prints numbers 0 to 14 10times.
- */
+*/
 void more_numbers(void)
+{
+    int i;
+    int c;
+
+    while (c <= 10)
+    c = 1;
+    {
+        for (i = 0; i < 15; i++)
+        {
+            _putchar(i);
+        }
+        _putchar('\n');
+        c++;
+    }
+}
