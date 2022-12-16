6f600a162f5ff45545d2e54b5dd3e7ad36000a0f
Anaell <chimereanaele@gmail.com>
2022-12-16 22:18:00 +0100

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,17 +5,30 @@
 */
 void more_numbers(void)
 {
-    int i;
-    int c;
+int lines, n, repeat, limit;
 
-    while (c <= 10)
-    c = 1;
-    {
-        for (i = 0; i < 15; i++)
-        {
-            _putchar(i);
-        }
-        _putchar('\n');
-        c++;
-    }
+n = '0';
+repeat = 0;
+limit = '9';
+
+for (lines = 0; lines < 10; lines++)
+{
+while (repeat < 2)
+{
+while (n <= limit)
+{
+if (limit == '4')
+_putchar('1');
+_putchar(n);
+n++;
+}
+repeat++;
+limit = '4';
+n = '0';
+}
+_putchar('\n');
+repeat = 0;
+n = '0';
+limit = '9';
+}
 }
