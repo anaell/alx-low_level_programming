03baa51c0453d4fed4ca110ccbaf7db2859ca97b
Anaell <chimereanaele@gmail.com>
2022-12-16 21:39:26 +0100

    new

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
 /**
- * 
-*/
+ * print_numbers - this function print numbers 0 to 9.
+ */
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+_putchar(i);
+}   
+}
