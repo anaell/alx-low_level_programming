c043a8062f477f7ef43a271b7de634bb95490ee1
Anaell <chimereanaele@gmail.com>
2022-12-16 20:19:11 +0100

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,31 +6,27 @@
 */
 void times_table(void)
 {
-    int a;
-    int b;
-    int c;
+int a;
+int b;
+int c;
+int tens;
+int units;
 
-    for (a = 0; a <= 9; a++)
-    {
-        for (b = 0; b <= 9; b++)
-        {
-            if (c < 10)
-            {
-                c = a * b;
-                _putchar(c + '0');
-                _putchar(',');
-                _putchar(' ');
-            }
-            else
-            {
-                c = a * b;
-                _putchar(c + '\0');
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
 
-        _putchar('\n');
-    
-    }
+c = a * b;
+tens = c / 10;
+units = c % 10;
+_putchar(tens + '0');
+_putchar(units + '0');
+_putchar(',');
+_putchar(' ');
+}
+
+_putchar('\n');
+
+}
 }
