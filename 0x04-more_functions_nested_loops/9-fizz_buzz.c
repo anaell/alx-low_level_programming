5e2635aaa55f549fdb0a74c8b92871ced74f69ef
Anaell <chimereanaele@gmail.com>
2022-12-16 23:09:50 +0100

    new

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - this program solves the fizzbuzz test.
+ * 
+ * Return: (0)
+*/
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0)
+        {
+            _putchar('Fizz');
+        }
+        else if (i % 5 == 0)
+        {
+            _putchar('Buzz');
+            _putchar()
+        }
+        else if (i % 3 == 0 && i % 5 == 0)
+        {
+            _putchar('FizzBuzz');
+        }
+        else
+        {
+            _putchar(i);
+        }        
+    }    
+}
