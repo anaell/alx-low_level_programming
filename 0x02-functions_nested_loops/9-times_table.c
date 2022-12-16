1f99e370397b60be9b982829583cf5c3cb25d123
Anaell <chimereanaele@gmail.com>
2022-12-16 20:33:02 +0100

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,34 +13,32 @@ int tens;
 int units;
 
 for (a = 0; a <= 9; a++)
-{
-    for (b = 0; b <= 9; b++)
-    {
-        c = a * b;
-        tens = c / 10;
-        units = c % 10;
-        if (c == 0)
-        {
-                _putchar('0');
-        }
-        
-        else if (c < 10)
-        {
-            _putchar(',');
-            _putchar(' ');
-            _putchar(' ');
-            _putchar(units);
-        }
-        else
-        {
-            _putchar(',');
-            _putchar(' ');
-            _putchar(tens + '0');
-            _putchar(units + '0');
-        }
-    }
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			tens = c / 10;
+			units = c % 10;
 
-    _putchar('\n');
-
-    }
+			if (b == 0)
+			{
+				_putchar('0');
+			}
+			else if (c < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(units + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(tens + '0');
+				_putchar(units + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
