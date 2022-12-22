4b339418a132ba1e1b8d2551d36657b6520966ca
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-21 20:59:00 -0800

    new

---
+++ 0x05-pointers_arrays_strings/0-reser_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reser_to_98 - this functions takes a pointer to an int as
+ * parameter and updates the value it points to 98.
+ *
+ * @n: the pointer.
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
