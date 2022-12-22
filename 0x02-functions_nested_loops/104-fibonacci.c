c2df15217303fb4a4dd312423505bf3b41046638
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-21 20:33:58 -0800

    new

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,17 +8,85 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int sum, num1 = 0, num2 = 1;
+	
+	int count;
+	
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	
+	unsigned long half1, half2;
+	
 
-	for (i = 0; i < 98; i++)
+	
+	for (count = 0; count < 92; count++)
+		
 	{
-		sum = num1 + num2;
-		printf("%lu", sum);
-		num1 = num2;
-		num2 = sum;
-		if (i < 97)
-			printf(", ");
+		
+		sum = fib1 + fib2;
+		
+		printf("%lu, ", sum);
+		
+
+		
+		fib1 = fib2;
+		
+		fib2 = sum;
+		
 	}
+	
+
+	
+	fib1_half1 = fib1 / 10000000000;
+	
+	fib2_half1 = fib2 / 10000000000;
+	
+	fib1_half2 = fib1 % 10000000000;
+	
+	fib2_half2 = fib2 % 10000000000;
+	
+
+	
+	for (count = 93; count < 99; count++)
+		
+	{
+		
+		half1 = fib1_half1 + fib2_half1;
+		
+		half2 = fib1_half2 + fib2_half2;
+		
+		if (fib1_half2 + fib2_half2 > 9999999999)
+			
+		{
+			
+			half1 += 1;
+			
+			half2 %= 10000000000;
+			
+		}
+		
+
+		
+		printf("%lu%lu", half1, half2);
+		
+		if (count != 98)
+			
+			printf(", ");
+		
+
+		
+		fib1_half1 = fib2_half1;
+		
+		fib1_half2 = fib2_half2;
+		
+		fib2_half1 = half1;
+		
+		fib2_half2 = half2;
+		
+	}
+	
+	printf("\n");
+	
 	return (0);
+	
 }
