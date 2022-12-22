70d20aa6136473c378f9e5c87cff1541e2bc3614
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-21 20:22:02 -0800

    new

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - this funtion prints the first 98 fibonacci numbers
+ * starting with 1 and 2.
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int sum, num1 = 0, num2 = 1;
+
+	for (i = 0; i < 98; i++)
+	{
+		sum = num 1 + num2;
+		printf("%lu", sum);
+		num1 = num2;
+		num2 = sum;
+		if (i < 97)
+			printf(", ");
+	}
+	return (0);
+}
