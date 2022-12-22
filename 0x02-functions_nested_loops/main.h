b7205974d396bfd4c770046e5a57490011b6a49b
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-21 18:31:06 -0800

    new

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,6 @@ int add(int a, int b);
 int print_last_digit(int x);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
