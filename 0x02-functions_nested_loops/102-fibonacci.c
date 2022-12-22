059864a39c8da7e5146a4fae217b9f0ce6d77181
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-21 19:48:11 -0800

    new

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints the first 50 fibonacci numbers
+ * starting with 1 and 2.
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int num1, num2, num3;
+
+	num1 = 1;
+	num2 = 2;
+	printf("%d, ", num1);
+	printf("%d, ", num2);
+	for (i = 0; i < 50; i++)
+	{
+		num3 = num1 + num2;
+		printf("%lu", num3);
+		
+		num1 = num2;
+		num2 = num3;
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
