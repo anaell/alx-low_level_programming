80db8fdd41a0eba211d0055cb60436562a5f8012
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-21 20:39:32 -0800

    new

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,11 +17,11 @@ int main(void)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
-		
+
 		fib1 = fib2;
 		fib2 = sum;
 	}
-	
+
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -36,11 +36,11 @@ int main(void)
 			half1 += 1;
 			half2 %= 10000000000;
 		}
-		
+
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
-		
+
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
