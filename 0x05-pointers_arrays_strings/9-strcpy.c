55345b3fae631504ed3e32c17738b7180621ba6d
Anaell <chimereanaele@gmail.com>
2022-12-24 02:57:27 +0100

    new

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,9 +12,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' || src[i] == '\0'; i++)
+	for (i = 0; *(src + i) != '\0' || src[i] == '\0'; i++)
 	{
-		return (*(dest + i) = *(src + i));
+		dest[i] = *(src + i);
 	}
-	
+	return (dest);
 }
