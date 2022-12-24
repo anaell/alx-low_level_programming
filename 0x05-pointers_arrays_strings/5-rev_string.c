924054918f8186fdd081e0bfc0d03b80b7911378
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-23 19:35:27 -0800

    new

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,21 @@
  */
 void rev_string(char *s)
 {
-	int i, a;
+	char end_char, first_char;
+	int i, end_char_posi, first_char_posi;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{}
 
-	a = i;
-	for (a -= 1; a <= 0; a--)
+	end_char_posi = i - 1;
+	first_char_posi = end_char_posi / 2;
+
+	while(first_char_posi >= 0)
 	{
-		char str[] += s[a];
+		first_char = s[end_char_posi - first_char_posi];
+		end_char = s[first_char_posi];
+		s[first_char_posi] = first_char;
+		s[end_char_posi - first_char_posi] = end_char;
+		first_char_posi--;
 	}
-	s = str;
 }
