fa922b64468beb9e82a2feb5902c23ee06fd9ae9
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-23 17:53:57 -0800

    new

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - this function prints a string in reverse.
+ *
+ * @s: the string to be printed in reverse.
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, a;
+	
+	for (i = 0; *s != '\0'; i++)
+	{}
+
+	a = i;
+
+	for (a = i; a >= 0; a--)
+	{
+		_putchar(*(s + a));
+	}
+}
