c259aa4622185c7363ecf0f59a53999b2bf21696
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-23 18:26:41 -0800

    new

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - this function reverses a string.
+ *
+ * @s: string to be reversed.
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char str[] = "";
+	int i, a;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+
+	a = i;
+	for (a -= 1; a <= 0; a--)
+	{
+		str[] += s[a];
+	}
+	s = str;
+}
