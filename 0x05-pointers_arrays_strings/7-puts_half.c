c97017e34f879c6c92dab4c9879d78e4b7d2e604
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-23 20:33:20 -0800

    new

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,5 +9,33 @@
  */
 void puts_half(char *str)
 {
+	int i, a, last, half;
+	char last_part, half_char;
+	
+	for (i = 0; str[i] != '\0'; i++)
+	{}
 
+	last = i - 1;
+
+	if (last % 2 == 0)
+	{
+		half = last / 2;
+
+		while (half <= last)
+		{
+			_putchar(str[half]);
+			half++;
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		half = (last - 1) / 2;
+		while (half <= last)
+		{
+			_putchar(str[half]);
+			half++;
+		}
+		_putchar('\n');
+	}
 }
