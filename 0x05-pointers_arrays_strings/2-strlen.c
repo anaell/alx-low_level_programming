873cc3e3211f3a48448e895205eb20b060fbd658
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-23 16:09:06 -0800

    new

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - this function returns the length of a string.
+ *
+ * @s: the string to find length.
+ *
+ * Return: (0)
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
