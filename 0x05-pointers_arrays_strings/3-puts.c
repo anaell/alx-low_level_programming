518fc3a67baec097b49057698919db0f458b45dc
Anaele Chimeremma <chimereanaele@gmail.com>
2022-12-23 16:56:33 -0800

    new

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - this function prints a string to stdout.
+ *
+ * @str: the string to print out.
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
