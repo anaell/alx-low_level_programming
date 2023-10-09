56c84aac2ee025b40131900f15a98b6608ff61ef
chimeremma <chimereanaele@gmail.com>
2023-10-09 16:44:18 -0700

    created anew file adn put some C code in it

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the is a description for main
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
