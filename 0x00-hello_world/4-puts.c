3baba03b06ef43890bd2e3772ec562e8b2ac2472
chimeremma <chimereanaele@gmail.com>
2023-10-09 16:37:19 -0700

    added a comment description to the main function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-
+/**
+ * main - the is a description for main
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ *
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
