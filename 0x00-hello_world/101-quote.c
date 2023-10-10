259f11ff29c47eeade15ccef5ec14b913946a953
chimeremma <chimereanaele@gmail.com>
2023-10-09 17:34:25 -0700

    created new files and added some code to them

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the is a description for main
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 31);
+return (1);
+}
