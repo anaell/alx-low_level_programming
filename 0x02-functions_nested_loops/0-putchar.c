54755b2a180850d1885533dc8855d2eda49b25b4
chimeremma <chimereanaele@gmail.com>
2023-10-10 20:49:00 -0700

    created a new file main.h  and added some codes to it adn to 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - this function prints '_putchar'
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	char tarp[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(tarp[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
