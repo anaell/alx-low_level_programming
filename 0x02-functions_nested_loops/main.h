985d643e246dfc64c1d49c6d370b8a9540472522
chimeremma <chimereanaele@gmail.com>
2023-10-10 21:30:16 -0700

    updated the main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,16 @@
-int _putchar(char c);
+int _putchar(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int m);
+int _isalpha(int a);
+int print_sign(int e);
+int _abs(int f);
+int print_last_digit(int x);
+void jack_bauer(void);
+int add(int a, int b);
+int print_last_digit(int x);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
